eef2f712d3b63a7cca60eca5a05c962f4566a696
Takuji Nishibayashi <takuji31@gmail.com>
2016-11-22 19:19:33 +0900

    [from now] 2016/11/22 19:19:33
    diff --git a/lib/autocomplete-perl.js b/lib/autocomplete-perl.js
    index 68a90b9..c7ac93f 100644
    --- a/lib/autocomplete-perl.js
    +++ b/lib/autocomplete-perl.js
    @@ -1,5 +1,24 @@
     "use strict";
    -module.exports = {
    -    provider: null,
    -    activate: function () { }
    -};
    +var AutocompletPerlProvider = (function () {
    +    function AutocompletPerlProvider() {
    +        this.provider = null;
    +    }
    +    AutocompletPerlProvider.prototype.activate = function () {
    +    };
    +    AutocompletPerlProvider.prototype.deactivate = function () {
    +        this.provider = null;
    +    };
    +    AutocompletPerlProvider.prototype.provide = function () {
    +        if (this.provider === null) {
    +            //TODO initialize
    +            this.provider = new AutocompletPerlProvider();
    +        }
    +        return this.provider;
    +    };
    +    AutocompletPerlProvider.prototype.consumeSnippets = function (snippets) {
    +        if (this.provider !== null) {
    +        }
    +    };
    +    return AutocompletPerlProvider;
    +}());
    +module.exports = AutocompletPerlProvider;
    diff --git a/lib/autocomplete-perl.ts b/lib/autocomplete-perl.ts
    index 6ef8e31..68bf11a 100644
    --- a/lib/autocomplete-perl.ts
    +++ b/lib/autocomplete-perl.ts
    @@ -1,5 +1,23 @@
    -export = {
    -  provider : null,
    -  activate: function(){}
    +class AutocompletPerlProvider {
    +  provider: AutocompletPerlProvider = null
    +  activate() {
    
    +  }
    +  deactivate() {
    +    this.provider = null
    +  }
    +  provide() {
    +    if (this.provider === null) {
    +      //TODO initialize
    +      this.provider = new AutocompletPerlProvider()
    +    }
    +    return this.provider
    +  }
    +
    +  consumeSnippets(snippets)  {
    +    if (this.provider !== null) {
    +      // this.provider.setSnippetsSource(snippets)
    +    }
    +  }
     }
    +export = AutocompletPerlProvider
    diff --git a/lib/provider.js b/lib/provider.js
    new file mode 100644
    index 0000000..23686ea
    --- /dev/null
    +++ b/lib/provider.js
    @@ -0,0 +1,7 @@
    +"use strict";
    +var PerlAutocompleteProvider = (function () {
    +    function PerlAutocompleteProvider() {
    +    }
    +    return PerlAutocompleteProvider;
    +}());
    +module.exports = PerlAutocompleteProvider;
    diff --git a/lib/provider.ts b/lib/provider.ts
    new file mode 100644
    index 0000000..f49496c
    --- /dev/null
    +++ b/lib/provider.ts
    @@ -0,0 +1,4 @@
    +class PerlAutocompleteProvider {
    +
    +}
    +export = PerlAutocompleteProvider
    diff --git a/tsconfig.json b/tsconfig.json
    index abd572c..9cdeeb2 100644
    --- a/tsconfig.json
    +++ b/tsconfig.json
    @@ -12,7 +12,7 @@
       "formatCodeOptions": {
         "indentSize": 4,
         "tabSize": 4,
    -    "newLineCharacter": "\r\n",
    +    "newLineCharacter": "\n",
         "convertTabsToSpaces": true,
         "insertSpaceAfterCommaDelimiter": true,
         "insertSpaceAfterSemicolonInForStatements": true,

--- lib/autocomplete-perl.ts
+++ lib/autocomplete-perl.ts
@@ -1,5 +1,23 @@
-export = {
-  provider : null,
-  activate: function(){}
+class AutocompletPerlProvider {
+  provider: AutocompletPerlProvider = null
+  activate() {
 
+  }
+  deactivate() {
+    this.provider = null
+  }
+  provide() {
+    if (this.provider === null) {
+      //TODO initialize
+      this.provider = new AutocompletPerlProvider()
+    }
+    return this.provider
+  }
+
+  consumeSnippets(snippets)  {
+    if (this.provider !== null) {
+      // this.provider.setSnippetsSource(snippets)
+    }
+  }
 }
+export = AutocompletPerlProvider
