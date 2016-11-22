3d6d33792b51cd02571a81f431f1288da2ce986f
Takuji Nishibayashi <takuji31@gmail.com>
2016-11-22 18:46:56 +0900

    initial commit

---
+++ spec/autocomplete-perl-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AutocompletePerlView from '../lib/autocomplete-perl-view';
+
+describe('AutocompletePerlView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
